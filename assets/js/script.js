60a076e905363dca0d1c868363909186b528ee91
Hannah Lee <013hannah.a.lee@gmail.com>
2021-03-10 23:23:37 -0600

    added a favorite button with an event listener. Clicking it will bring up a toast, which informs the usser the button has been clicked. Card will now populate with quote, author, and name of font family

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,10 @@
 var quoteAPI = 'https://quote-garden.herokuapp.com/api/v3/quotes'
 var fontAPI = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyApTTfxzWtDTJAFO8DS8W5vBBqqPCCKmUs'
 var quoteDisplayEl = document.querySelector('.quote');
+var familyDisplayEl = document.querySelector('.font-name');
 
+
+//Fetch information from Google fonts API
 fetch(fontAPI)
   .then(function (response) {
     return response.json();
@@ -15,8 +18,13 @@ fetch(fontAPI)
     // family
     console.log(data.items[1].family);
 
+    fontFamily = data.items[1].family;
+
+    displayFont(fontFamily);
+
 });
 
+//Fetch information from Quote Garden API
 fetch(quoteAPI)
   .then(function (response) {
     return response.json();
@@ -25,12 +33,30 @@ fetch(quoteAPI)
       console.log(data.data[1].quoteText);
       console.log(data.data[1].quoteAuthor);
 
+      quoteAuthor = data.data[1].quoteAuthor;
+      quoteText = data.data[1].quoteText;
+      quoteInfo = quoteText + " -" + quoteAuthor;
+
+      displayQuote(quoteInfo);
+      
+
 });
 
-function displayQuote(quoteText, quoteAuthor) {
-
-quoteDisplayEl.textContent = quoteText + " -" + quoteAuthor
-
+//Functions to display API information on card
+function displayFont (fontFamily) {
+    familyDisplayEl.textContent = fontFamily;
 }
 
-displayQuote(data.data[1].quoteText, data.data[1].quoteAuthor)
+function displayQuote(quoteInfo) {
+    quoteDisplayEl.textContent = quoteInfo;
+}
+
+//Function to save information
+function saveFont() {
+    M.toast({html: 'Clicked!'})
+};
+
+//Favorite button for user to save information
+document.getElementById('favorite-btn').addEventListener("click", saveFont);
+
+displayQuote();      
