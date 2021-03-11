36f5995e0a9d3dee69646047869d7429e97d95ff
BarbaraSheaKenny <b.kenny58@gmail.com>
2021-03-10 23:14:09 -0500

    updated javascript--function to populate quote and font to cards

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 var quoteAPI = 'https://quote-garden.herokuapp.com/api/v3/quotes'
 var fontAPI = 'https://www.googleapis.com/webfonts/v1/webfonts?key=AIzaSyApTTfxzWtDTJAFO8DS8W5vBBqqPCCKmUs'
 var quoteDisplayEl = document.querySelector('.quote');
+var fontNameDisplay = document.querySelector('.font-name');
 
 fetch(fontAPI)
   .then(function (response) {
@@ -14,7 +15,8 @@ fetch(fontAPI)
     console.log(data.items[1].category);
     // family
     console.log(data.items[1].family);
-
+    
+    displayFont(data.items[1].family, data.items[1].category);
 });
 
 fetch(quoteAPI)
@@ -22,15 +24,30 @@ fetch(quoteAPI)
     return response.json();
   })
   .then(function (data) {
-      console.log(data.data[1].quoteText);
-      console.log(data.data[1].quoteAuthor);
+    // quote
+    console.log(data.data[1].quoteText);
+    //author
+    console.log(data.data[1].quoteAuthor);
+
+displayQuote(data.data[1].quoteText, data.data[1].quoteAuthor)
 
 });
 
+
+//displays font name and category to card--- need to add randomizer
+function displayFont(fontFamily, fontCategory) {
+
+    fontNameDisplay.textContent = fontFamily + ", " + fontCategory;
+    
+    }
+
+//displays quote to card--- need to add randomizer
 function displayQuote(quoteText, quoteAuthor) {
 
-quoteDisplayEl.textContent = quoteText + " -" + quoteAuthor
+    quoteDisplayEl.textContent = quoteText + " -" + quoteAuthor;
+    
+    }
 
-}
 
-displayQuote(data.data[1].quoteText, data.data[1].quoteAuthor)
+
+
