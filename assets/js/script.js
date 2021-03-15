e2c43f0dd8eb2e2a8af9f8bf626efebe6f25268e
Ryan Neil Angeles Vega <vegaryanneil@gmail.com>
2021-03-15 19:41:19 -0400

    Added Sidebar nav when when screen is small

--- assets/js/script.js
+++ assets/js/script.js
@@ -165,3 +165,16 @@ document.getElementById('btn-up').addEventListener("click", renderNewSave);
 displayFont();
 displayQuote();
 
+// Sidebar Navigation
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems);
+  });
+
+var collapsibleElem = document.querySelector('.collapsible');
+var collapsibleInstance = M.Collapsible.init(collapsibleElem);
+
+// $(document).ready(function(){
+//     $('.sidenav').sidenav();
+//   });
